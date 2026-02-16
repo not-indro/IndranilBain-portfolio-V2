1da8ae9cd18803b4b35e4e0f5656949a7c3a1728
[Indranil Bain] <indranilbain@gmail.com>
2026-02-16 18:35:24 +0530

    Fix SVG loading for Simple Icons

--- next.config.mjs
+++ next.config.mjs
@@ -12,6 +12,8 @@ const nextConfig = {
         hostname: "cdn.simpleicons.org",
       },
     ],
+    dangerouslyAllowSVG: true,
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
 };
 
