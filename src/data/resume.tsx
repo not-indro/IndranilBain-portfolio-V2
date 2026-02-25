6a6e8f641e92f907fe91fb6d714e8b86870d6ffb
[Indranil Bain] <indranilbain@gmail.com>
2026-02-25 17:35:39 +0530

    Update project images and resume data

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -372,7 +372,7 @@ export const DATA = {
           icon: <Icons.globe className="size-3" />,
         },
       ],
-      image: "/skincurate.png",
+      image: "/skincurate.jpg",
       video: "",
     },
     {
